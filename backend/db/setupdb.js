61b01a9ba216eeb23f33f8d99bfa6e9474977293
Daois Sanchez Mora <daois@Mac.home>
2024-11-28 22:55:14 -0700

    added these things idek i forgot

---
+++ backend/db/setupdb.js
@@ -0,0 +1,20 @@
+import { readFileSync } from "fs";
+import { dirname, join } from "path";
+import { pool } from "./pool.js";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filepath = join(__dirname, "init.sql");
+
+const setupdb = async () => {
+    try {
+        const sql = readFileSync(filepath, "utf8");
+        await pool.query(sql);
+        console.log("Database was created successfully");
+    } catch (error) {
+        console.error("An error has occurred when created database:", error);
+    }
+};
+
+export { setupdb };
