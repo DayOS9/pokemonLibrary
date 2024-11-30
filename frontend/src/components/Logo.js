c83e2700a7c7a4aa6a5d75cea6398e34ca30b1f0
Daois Sanchez Mora <daois@Mac.home>
2024-11-30 14:32:11 -0700

    added the logo to be used in the header/title

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from "react"
+import logo from "./images/pokeball.png"
+
+function Logo() {
+    return (
+        <img src={logo} alt="Pokeball img" height={200} width={200} />
+    )
+}
+
+export default Logo;
