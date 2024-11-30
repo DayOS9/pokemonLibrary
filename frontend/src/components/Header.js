bcd65ac4b83888546949e0cf4c568b7418087562
Daois Sanchez Mora <daois@Mac.home>
2024-11-30 14:31:48 -0700

    added the title and our names

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,11 @@ import React from "react";
 function Header() {
     return (
         <header className="header">
-            Pokemon
+            <h1>Pokemon Library</h1>
+            Daois Sanchez Mora<br />
+            Justine Sanchez Mora<br />
+            Alvin Wang<br />
+            Ryan Fernandes<br />
         </header>
     )
 }
