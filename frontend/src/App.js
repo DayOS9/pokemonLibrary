40e54b7319d717e73b4dfc03f1f650db295a6cfa
Daois Sanchez Mora <daois@Mac.home>
2024-11-30 14:30:55 -0700

    added logo and title together

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from "./components/Header"
+import Logo from "./components/Logo"
 
 function App() {
     return (
         <div>
+            <Logo />
             <Header />
         </div>
     );
