c5c4a9d3c6f148c8e96713933da57470a903ee18
Daois Sanchez Mora <daois@Mac.home>
2024-11-30 17:31:09 -0700

    changed header so it does not include css

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-        <header className="header">
+        <header >
             <h1>Pokemon Library</h1>
             Daois Sanchez Mora<br />
             Justine Sanchez Mora<br />
