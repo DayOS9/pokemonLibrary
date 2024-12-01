b25d77880f87700cbfbb8ef90f96cc648d6eb08c
Daois Sanchez Mora <daois@Mac.home>
2024-11-30 17:30:14 -0700

    added the component to main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Header from "./components/Header"
 import Logo from "./components/Logo"
+import SearchBar from './components/SearchBar';
 
 function App() {
     return (
-        <div>
-            <Logo />
-            <Header />
+        <div className='App'>
+            <div className="SearchBoxContainer">
+                <div className='Header'>
+                    <Logo />
+                    <Header />
+                </div>
+                <SearchBar />
+            </div>
         </div>
     );
 }
